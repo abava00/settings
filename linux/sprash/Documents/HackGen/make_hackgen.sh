fa77cd357e1ba2197253daa64697cbce7db1a8df
あ馬場 <60212516+abava00@users.noreply.github.com>
2022-04-27 17:19:43 +0900

    Add files via upload
    temp

---
+++ linux/sprash/Documents/HackGen/make_hackgen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+PREFIX="$1"
+
+function mvBuild() {
+  mkdir -p "${BASE_DIR}/build/"
+  mv -f "${BASE_DIR}/"HackGen*.ttf "${BASE_DIR}/build/"
+}
+
+"${BASE_DIR}/hackgen_generator.sh" "$PREFIX" \
+&& "${BASE_DIR}/os2_patch.sh" "$PREFIX" \
+&& mvBuild
