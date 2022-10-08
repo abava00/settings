b8d031168992cdbd76e307b57aedc1673faa7f01
abava00 <60212516+abava00@users.noreply.github.com>
2022-10-08 23:35:42 +0900

    設定ファイルの再構築(未完成)

---
+++ emacsfiles/.emacs.d/init.el
@@ -0,0 +1,79 @@
+;#$#$#$#$#$MySetting
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; タブにスペースを使用する
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+; 色のカスタマイズ
+(custom-set-faces
+'(hl-line ((t (:background "gray41"))))
+)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+;(set-face-background 'show-paren-match-face "grey")
+;(set-face-foreground 'show-paren-match-face "black")
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+
+;;; dired設定
+(require 'dired-x)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;;ファイルがどこで終わるのかわかるようになる
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left);#$#$#$#$#$
+
+;フォントの設定
+(add-to-list 'default-frame-alist
+                       '(font . "Cascadia Code PL-12"))
